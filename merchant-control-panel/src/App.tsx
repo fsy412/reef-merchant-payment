bc4144f887c8600b8b011cb5c95f645c3b5af6e2
fsy412 <fsy412@gmail.com>
2022-04-03 00:55:36 +0800

    up

--- merchant-control-panel/src/App.tsx
+++ merchant-control-panel/src/App.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
-import Login from './pages/Login'
 import Nav from './components/Nav'
 import { BrowserRouter, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Register from './pages/Register'
 import Pannel from './pages/Pannel'
 
 function App() {
@@ -20,9 +17,6 @@ function App() {
       <BrowserRouter>
         <Nav name={name} setName={setName} />
         <Route path="/" exact component={() => <Pannel name={name} />} />
-        <Route path="/login" component={() => <Login setName={setName} />} />
-        <Route path="/register" component={Register} />
-        <Route path="/home" component={Home} />
       </BrowserRouter>
     </div>
   )
--- merchant-control-panel/src/components/Nav.tsx
+++ merchant-control-panel/src/components/Nav.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Nav = (props: { name: string, setName: (name: string) => void }) => {
+const Nav = (props: {
+    name: string,
+    setName: (name: string) => void
+}
+) => {
     const logout = async () => {
         await fetch('http://localhost:8000/api/logout', {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
         });
 
